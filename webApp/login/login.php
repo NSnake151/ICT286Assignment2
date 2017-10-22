962504d474b45b521006d9df4118db23afc6cac9
NSnake151 <19525940+NSnake151@users.noreply.github.com>
2017-10-22 16:51:07 +0800

    Add files by upload

---
+++ webApp/login/login.php
@@ -0,0 +1,11 @@
+<?php
+
+	$host = "localhost";
+	$user = "X31873958";
+	$password = "DayOneIsH2020";
+	$dbc = mysql_pconnect($host, $user, $password);
+	$dbname = "X31873958";	
+	mysql_select_db($dbname);
+
+
+?>
